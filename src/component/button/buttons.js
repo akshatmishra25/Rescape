3522e7076b1b850b4d1cc220e43aef6372116851
Ujjwal Gupta <guptaujjwal259@gmail.com>
2022-12-26 12:40:54 +0530

    fifth commit

--- src/component/button/buttons.js
+++ src/component/button/buttons.js
@@ -4,17 +4,22 @@ import "./style.css"
 
 import {useAuth0} from "@auth0/auth0-react"
 const Buttons = () => {
-  const { loginWithRedirect ,isAuthenticated } = useAuth0();
+  const { loginWithRedirect ,logout,isAuthenticated} = useAuth0();
   return ( 
-    !isAuthenticated&&(
-<div>
-     
-     <button class="button2" onClick={() => loginWithRedirect()}>
-         <span></span>
-          LOG IN
-        </button>
-     </div>
-    )
+    isAuthenticated ?(<div>
+      <button className="button2" onClick={() => logout({ returnTo: window.location.origin })}>
+      <span></span>
+    LOG OUT
+  </button>
+  </div>)
+    
+        
+        :(<div>
+          <button class="button2" onClick={() => loginWithRedirect()}>
+              <span></span>
+               LOG IN
+             </button> 
+             </div>)
    
   )
 } 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
